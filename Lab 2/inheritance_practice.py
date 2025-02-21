b6c4126d05a263f6de406df8b124d4062cb94e20
JevTheBone <kevinavina12@hotmail.com>
2025-02-21 00:26:08 -0800

    Changes to be committed: 	modified:   Lab 2/inheritance_practice.py 	new file:   Lab 2/tests/inheritance_tests.py 	new file:   Lab 2/tests/pytest.ini

--- Lab 2/inheritance_practice.py
+++ Lab 2/inheritance_practice.py
@@ -2,18 +2,18 @@
 Author: Kevin Avina-Gutierrez
 Description: Practicing inheritance and 
     other OOP paradigms
-Date: 02/14/2025
+Date: 02/20/2025
 """
 
 # Class defining a generic animal
 class Animal:
-    def __init__(self, name, hungry, temperment):
+    def __init__(self, name, hungry = False, temperment = "unknown"):
         # Name of the animal
-        animal_name = name
+        self.animal_name = name
         # Boolean representing if the animal is hungry
-        hungry = False
-        # Temperment of the animal
-        animal_temperment = temperment
+        self.hungry = hungry
+        # Temperment of the animal, is it friendly or not?
+        self.temperment = temperment
         pass 
 
     # Print what the animal "says"
@@ -22,13 +22,44 @@ class Animal:
 
     # Make the animal sleep
     def sleep(self):
-        print("{animal_name} is sleeping...zzzZZZzzz")
+        print(f"{self.animal_name} is sleeping...zzzZZZzzz")
+        # Set animal hunger to True
+        self.hungry = True
+
+    # Make the animal eat
+    def eat(self, food):
+        # Check if animal is hungry
+        if self.hungry == True:
+            print(f"{self.animal_name} ate {food}!")
+            # Set animal to not hungry, since it just ate
+            self.hungry = False
+            return True
+        
+        # Animal is not hungry, return false
+        else:
+            print(f"{self.animal_name} did not eat {food}...")
+
+            return False
+    
+    # Is the animal friendly?
+    def is_friendly(self):
+        if self.temperment == "friendly":
+            print(f"{self.animal_name} is friendly!")
+            return True
+        # Add statements for various temperments
+        else:
+            print(f"{self.animal_name} is not friendly... watch out!")
+            return False
 
 def run():
     # Initialize an animal
-    an_animal = Animal()
+    an_animal = Animal("T-rex")
     # Make the animal speak
     an_animal.speak()
+    # Make the animal sleep
+    an_animal.sleep()
+    # Make the animal eat
+    an_animal.eat("a person")
 
 if __name__ == "__main__":
     run()
---
+++ Lab 2/tests/inheritance_tests.py
@@ -0,0 +1,82 @@
+""" 
+Author: Kevin Avina-Gutierrez
+This is a test file for inheritance.py 
+    It tests the Animal class and its methods
+Date: 02/20/2025
+"""
+
+import pytest
+from inheritance_practice import Animal
+
+##########################################################
+# To run the test you must use the command:              #
+#   py -m pytest -v inheritance_test.py(check directory) #
+#   py -m pytest -v -m "method"_test inheritance_test.py #
+##########################################################
+
+    ##########################
+    # Tests for eat() method #
+    ##########################
+
+@pytest.mark.eat_tests
+# Test if the animal ate
+def test_eat():
+    # Initialize an animal
+    an_animal = Animal("Dog", True, "friendly")
+    # Make the animal eat
+    an_animal.eat("dog food")
+    # Check that the animal is no longer hungry
+    assert an_animal.hungry == False
+
+@pytest.mark.eat_tests    
+# Test if the animal didn't eat
+def test_not_eat():
+    # Initialize an animal
+    an_animal = Animal("Dog", False, "friendly")
+    # Make the animal eat
+    result = an_animal.eat("dog food")
+    # Check if the eat method returned False
+    assert result == False
+
+    ##########################
+    # End of tests for eat() #
+    ##########################
+    
+    ##################################
+    # Tests for is_friendly() method #
+    ##################################
+
+@pytest.mark.is_friendly_tests
+# Test if the animal is friendly
+def test_is_friendly():
+    # Initialize an animal
+    an_animal = Animal("Dog", True, "friendly")
+    # Check if the animal is friendly
+    result = an_animal.is_friendly()
+    # Check if the is_friendly method returned True
+    assert result == True
+
+@pytest.mark.is_friendly_tests
+# Test if the animal is not friendly
+def test_not_friendly():
+    # Initialize an animal
+    an_animal = Animal("Dog", True, "upset")
+    # Check if the animal is friendly
+    result = an_animal.is_friendly()
+    # Check if the is_friendly method returned False
+    assert result == False
+
+    ######################################
+    # End tests for is_friendly() method #
+    ######################################
+
+    ##############################
+    # Tests for  method          #
+    ##############################
+
+#@pytest.mark.
+#def test_():
+    
+    ##############################
+    # Tests for  method          #
+    ##############################
